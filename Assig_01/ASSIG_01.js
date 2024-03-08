8893f659f16b32d7fbed8e2b1638be7a8fb58ad1
aftab-munir <159930558+aftab-munir@users.noreply.github.com>
2024-03-08 16:05:25 +0500

    Assig_V1
    Assignment No1

---
+++ Assig_01/ASSIG_01.js
@@ -0,0 +1,19 @@
+// assignement Percentage and Grade Caluclator nested if else function//
+// Auther: Muhammad Aftab Munir
+//Date :5TH MAR 2024
+var Percentage = '78';
+if (Percentage >= '90') {
+    console.log("YOU SECURE A++");
+}
+else if (Percentage >= "80") {
+    console.log("YOU SECURE A");
+}
+else if (Percentage >= "70") {
+    console.log(" YOU SECURE B");
+}
+else if (Percentage >= "60") {
+    console.log("you SECURE C");
+}
+else {
+    console.log("Fail");
+}
